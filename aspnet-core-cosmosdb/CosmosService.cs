d61aa07f55665c78bf420551343bc55e04403110
Cenk SARI <cenk@cenksari.com>
2025-12-09 11:50:06 +0000

    Refactor CosmosService, update .NET and package versions
    Refactored CosmosService for clarity, improved comments, and used more specific exceptions. Updated configuration key names and removed IDisposable. Reformatted ICosmosService for consistency. Changed target framework to .NET 10.0 and bumped NuGet package versions.

--- aspnet-core-cosmosdb/CosmosService.cs
+++ aspnet-core-cosmosdb/CosmosService.cs
@@ -7,148 +7,144 @@ using System.Net;
 using System.Threading.Tasks;
 
 /// <summary>
-/// Cosmos DB service.
+/// Represents Cosmos DB service.
 /// </summary>
-public class CosmosService(IConfiguration configuration) : ICosmosService, IDisposable
+public class CosmosService(IConfiguration configuration) : ICosmosService
 {
-	private const string DatabaseName = "ENTER_YOUR_COSMOS_DB_NAME_HERE";
+    /// <summary>
+    /// Database name.
+    /// </summary>
+    private const string DatabaseName = "ENTER_YOUR_COSMOS_DB_NAME_HERE";
 
-	private readonly CosmosClient _cosmosClient = new(
-		configuration["CosmosDb:Endpoint"] ?? throw new Exception("CosmosDb:Endpoint key configuration not found!"),
-		configuration["CosmosDb:Key"] ?? throw new Exception("CosmosDb:Key key configuration not found!"),
-		new()
-		{
-			PortReuseMode = PortReuseMode.PrivatePortPool,
-			SerializerOptions = new() { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase }
-		});
+    /// <summary>
+    /// Cosmos client.
+    /// </summary>
+    private readonly CosmosClient _cosmosClient = new(
+        configuration["ENTER_YOUR_COSMOS_DB_ENDPOINT_HERE"],
+        configuration["ENTER_YOUR_COSMOS_DB_KEY_HERE"],
+        new()
+        {
+            PortReuseMode = PortReuseMode.PrivatePortPool,
+            SerializerOptions = new() { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase }
+        });
 
-	/// <summary>
-	/// Gets container.
-	/// </summary>
-	/// <param name="containerName">Container name</param>
-	private Container GetContainer(string containerName) => _cosmosClient.GetContainer(DatabaseName, containerName);
+    /// <summary>
+    /// Gets container.
+    /// </summary>
+    /// <param name="containerName">Container name</param>
+    private Container GetContainer(string containerName) => _cosmosClient.GetContainer(DatabaseName, containerName);
 
-	/// <summary>
-	/// Validate params.
-	/// </summary>
-	/// <param name="args">Params</param>
-	private static void ValidateParams(params string[] args)
-	{
-		if (args.Any(string.IsNullOrWhiteSpace))
-			throw new Exception("Container name, Document ID or Query string cannot be null or empty.");
-	}
+    /// <summary>
+    /// Validate params.
+    /// </summary>
+    /// <param name="args">Params</param>
+    private static void ValidateParams(params string[] args)
+    {
+        if (args.Any(string.IsNullOrWhiteSpace))
+            throw new InvalidOperationException("Container name, Document ID or Query string cannot be null or empty.");
+    }
 
-	/// <summary>
-	/// Reads selected documents contents.
-	/// </summary>
-	/// <param name="containerName">Container name</param>
-	/// <param name="documentId">Document ID</param>
-	public async Task<T?> GetDocumentAsync<T>(string containerName, string documentId)
-	{
-		ValidateParams(containerName, documentId);
+    /// <summary>
+    /// Reads selected documents contents.
+    /// </summary>
+    /// <param name="containerName">Container name</param>
+    /// <param name="documentId">Document ID</param>
+    public async Task<T?> GetDocumentAsync<T>(string containerName, string documentId)
+    {
+        ValidateParams(containerName, documentId);
 
-		try
-		{
-			Container? container = GetContainer(containerName);
+        try
+        {
+            Container? container = GetContainer(containerName);
 
-			ItemResponse<T> response = await container.ReadItemAsync<T>(documentId, new(documentId));
+            ItemResponse<T> response = await container.ReadItemAsync<T>(documentId, new(documentId));
 
-			return response.Resource;
-		}
-		catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
-		{
-			return default;
-		}
-	}
+            return response.Resource;
+        }
+        catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+        {
+            return default;
+        }
+    }
 
-	/// <summary>
-	/// Creates a document.
-	/// </summary>
-	/// <param name="containerName">Container name</param>
-	/// <param name="item">Document</param>
-	public async Task<T> CreateDocumentAsync<T>(string containerName, T item)
-	{
-		ValidateParams(containerName);
+    /// <summary>
+    /// Creates a document.
+    /// </summary>
+    /// <param name="containerName">Container name</param>
+    /// <param name="item">Document</param>
+    public async Task<T> CreateDocumentAsync<T>(string containerName, T item)
+    {
+        ValidateParams(containerName);
 
-		Container? container = GetContainer(containerName);
+        Container? container = GetContainer(containerName);
 
-		ItemResponse<T> response = await container.CreateItemAsync(item);
+        ItemResponse<T> response = await container.CreateItemAsync(item);
 
-		return response.Resource;
-	}
+        return response.Resource;
+    }
 
-	/// <summary>
-	/// Returns all documents in selected collection.
-	/// </summary>
-	/// <param name="containerName">Container name</param>
-	/// <param name="queryString">Document query</param>
-	public async Task<IEnumerable<T>?> ListDocumentsAsync<T>(string containerName, string queryString)
-	{
-		ValidateParams(containerName, queryString);
+    /// <summary>
+    /// Returns all documents in selected collection.
+    /// </summary>
+    /// <param name="containerName">Container name</param>
+    /// <param name="queryString">Document query</param>
+    public async Task<IEnumerable<T>?> ListDocumentsAsync<T>(string containerName, string queryString)
+    {
+        ValidateParams(containerName, queryString);
 
-		Container? container = GetContainer(containerName);
+        Container? container = GetContainer(containerName);
 
-		FeedIterator<T> query = container.GetItemQueryIterator<T>(new QueryDefinition(queryString));
+        FeedIterator<T> query = container.GetItemQueryIterator<T>(new QueryDefinition(queryString));
 
-		List<T> results = [];
+        List<T> results = [];
 
-		while (query.HasMoreResults)
-		{
-			FeedResponse<T> response = await query.ReadNextAsync();
+        while (query.HasMoreResults)
+        {
+            FeedResponse<T> response = await query.ReadNextAsync();
 
-			results.AddRange(response);
-		}
+            results.AddRange(response);
+        }
 
-		return results;
-	}
+        return results;
+    }
 
-	/// <summary>
-	/// Replaces a document.
-	/// </summary>
-	/// <param name="containerName">Container name</param>
-	/// <param name="documentId">Document ID</param>
-	/// <param name="item">Document</param>
-	public async Task<T?> ReplaceDocumentAsync<T>(string containerName, string documentId, T item)
-	{
-		ValidateParams(containerName);
+    /// <summary>
+    /// Replaces a document.
+    /// </summary>
+    /// <param name="containerName">Container name</param>
+    /// <param name="documentId">Document ID</param>
+    /// <param name="item">Document</param>
+    public async Task<T?> ReplaceDocumentAsync<T>(string containerName, string documentId, T item)
+    {
+        ValidateParams(containerName);
 
-		Container? container = GetContainer(containerName);
+        Container? container = GetContainer(containerName);
 
-		ItemResponse<T> response = await container.UpsertItemAsync(item, new(documentId));
+        ItemResponse<T> response = await container.UpsertItemAsync(item, new(documentId));
 
-		return response.Resource;
-	}
+        return response.Resource;
+    }
 
-	/// <summary>
-	/// Deletes selected document.
-	/// </summary>
-	/// <param name="containerName">Container name</param>
-	/// <param name="documentId">Document ID</param>
-	public async Task<bool> DeleteDocumentAsync<T>(string containerName, string documentId)
-	{
-		ValidateParams(containerName, documentId);
+    /// <summary>
+    /// Deletes selected document.
+    /// </summary>
+    /// <param name="containerName">Container name</param>
+    /// <param name="documentId">Document ID</param>
+    public async Task<bool> DeleteDocumentAsync<T>(string containerName, string documentId)
+    {
+        ValidateParams(containerName, documentId);
 
-		try
-		{
-			Container? container = GetContainer(containerName);
+        try
+        {
+            Container? container = GetContainer(containerName);
 
-			ItemResponse<T> response = await container.DeleteItemAsync<T>(documentId, new(documentId));
+            await container.DeleteItemAsync<T>(documentId, new(documentId));
 
-			return true;
-		}
-		catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
-		{
-			return false;
-		}
-	}
-
-	/// <summary>
-	/// Disposes resources.
-	/// </summary>
-	public void Dispose()
-	{
-		_cosmosClient.Dispose();
-
-		GC.SuppressFinalize(this);
-	}
+            return true;
+        }
+        catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+        {
+            return false;
+        }
+    }
 }
--- aspnet-core-cosmosdb/ICosmosService.cs
+++ aspnet-core-cosmosdb/ICosmosService.cs
@@ -5,13 +5,13 @@ using System.Threading.Tasks;
 
 public interface ICosmosService
 {
-	Task<T> CreateDocumentAsync<T>(string containerName, T item);
+    Task<T> CreateDocumentAsync<T>(string containerName, T item);
 
-	Task<T?> GetDocumentAsync<T>(string containerName, string documentId);
+    Task<T?> GetDocumentAsync<T>(string containerName, string documentId);
 
-	Task<bool> DeleteDocumentAsync<T>(string containerName, string documentId);
+    Task<bool> DeleteDocumentAsync<T>(string containerName, string documentId);
 
-	Task<T?> ReplaceDocumentAsync<T>(string containerName, string documentId, T item);
+    Task<T?> ReplaceDocumentAsync<T>(string containerName, string documentId, T item);
 
-	Task<IEnumerable<T>?> ListDocumentsAsync<T>(string containerName, string queryString);
+    Task<IEnumerable<T>?> ListDocumentsAsync<T>(string containerName, string queryString);
 }
